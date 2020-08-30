be4ccde19c4d75f274fef0e5067e758de4956ade
FarshadRasa <rasafarshad@gmail.com>
2020-08-30 21:06:33 +0200

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Rainbow from '../hoc/Rainbow';
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dsf dsf dsf dsf sf sdf sdf dsf sdf sd fsd  df</p>
+        </div>
+    )
+}
+
+export default Rainbow(About);
---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = (props) => {
+    // setTimeout(() => {
+    //     props.history.push('/about')
+    // },2000);
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>asdf dsf sf sdf sdf dsf sdf sd fsd  df</p>
+        </div>
+    )
+}
+
+export default Contact;
